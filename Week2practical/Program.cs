c4d51f3e6f6c40d50d85db7d5cd6b9bdb3abb471
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-08 09:23:25 +0100

    task1

--- Week2practical/Program.cs
+++ Week2practical/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿PrintMenu();
+
+void PrintMenu()
+{
+    Console.WriteLine("Please enter a valid option from below:");
+    Console.WriteLine("1.Hello in Freanh");
+    Console.WriteLine("2.Hello in German?");
+    Console.WriteLine("4.Hello in Italian?");
+    Console.WriteLine("0. Exit application");
+
+}
+
+Main();
+
+ static void Main()
+{
+
+}
