63f408694c6dab3ee73b9edfdffb4b2e71e8e24a
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-16 19:11:45 +0100

    task1-4

--- Week2practical/Program.cs
+++ Week2practical/Program.cs
@@ -1,18 +1,95 @@
-﻿PrintMenu();
+﻿/*
+ * Practical 2
+ * Information: Methods demo
+ * Version 1
+ * Author : Eliza Ann Benoy
+ * Date: October
+ */
 
-void PrintMenu()
+static void PrintMenu()
 {
     Console.WriteLine("Please enter a valid option from below:");
     Console.WriteLine("1.Hello in Freanh");
-    Console.WriteLine("2.Hello in German?");
+    Console.WriteLine("2. Hello in Spanish?");
+    Console.WriteLine("3.Hello in German?");
     Console.WriteLine("4.Hello in Italian?");
     Console.WriteLine("0. Exit application");
 
 }
 
+
+
+static int InputOption()
+{
+    int Option;
+    try
+    {
+        Option = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+        Option = 0;
+
+    }
+    return Option;
+
+
+}
+
+static string GetMessage(int language)
+{
+    string msg = "";
+    switch( language )
+    {
+        case 0:
+            Console.WriteLine("Goodbye");
+            break;
+
+        case 1:
+            Console.WriteLine("Bonjour");
+            break;
+        case 2:
+
+            Console.WriteLine("Ola");
+            break;
+
+        case 3:
+            Console.WriteLine("Hallo");
+            break;
+        case 4:
+            Console.WriteLine("Ciao");
+            break;
+        default:
+            Console.WriteLine("Enter a vaild option");
+            break;
+
+
+
+    }
+    return msg;
+
+
+}
+
+
+
+
 Main();
 
  static void Main()
+
 {
+    string message = "";
+    do
+    {
+        PrintMenu();
+        int option = InputOption();
+        message = GetMessage(option);
+        Console.WriteLine(message);
+
+    } while (message != "Goodbye");
+
 
 }
+
